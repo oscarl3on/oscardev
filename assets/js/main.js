0cc92d299bd416936a70a04264e1a6080964529c
oscarl3on <oscarleon.alcatel@gmail.com>
2023-01-18 12:46:24 -0600

    seccion de cualidades finalizada

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,3 +48,32 @@ function toggleSkills(){
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
+
+// QUALIFICATIONS TABS
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tab.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
+
+
+
+
+// SERVICES MODAL
+// PORTFOLIO SWIPER
+// TESTIMONIAL
+// SCROLL SECTIONS ACTIVE LINK
+// CHANGE BAKCKROUND HEADER
